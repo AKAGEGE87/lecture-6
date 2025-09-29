5f0dc69d9b54c1625439e4fec32739d2ac5c7849
AKAGEGE87 <akagege87@gmail.com>
2025-09-29 19:25:01 +0400

    Lecture-6 1

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
 // Create three functions:
- function downloadMovie1() {
-    return new  Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Movie 1 downloaded");
-            
-        }, 1000); 
-    });
+function downloadMovie1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Movie 1 downloaded");
+    }, 1000);
+  });
 }
 
 function downloadMovie2() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Movie 2 downloaded");
-        }, 2000); 
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Movie 2 downloaded");
+    }, 2000);
+  });
 }
 function downloadMovie3() {
-    return  new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Movie 3 downloaded");
-        }, 3000); 
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Movie 3 downloaded");
+    }, 3000);
+  });
 }
 
 //    Part A - Sequential
-async function downloadPage(){
-const movie1 = await downloadMovie1()
-console.log(movie1)
+async function downloadPage() {
+  const movie1 = await downloadMovie1();
+  console.log(movie1);
 
-const movie2 = await downloadMovie2()
-console.log(movie2)
+  const movie2 = await downloadMovie2();
+  console.log(movie2);
 
-const movie3 = await downloadMovie3()
-console.log(movie3)
+  const movie3 = await downloadMovie3();
+  console.log(movie3);
 }
 
-downloadPage()
-
+downloadPage();
 
 // Part B – Parallel with Promise.all
 
-async function downloadAll(){
-const movieAll = await Promise.all([downloadMovie1, downloadMovie2, downloadMovie3])
-console.log(movieAll)
+async function downloadAll() {
+  const movieAll = await Promise.all([
+    downloadMovie1(),
+    downloadMovie2(),
+    downloadMovie3()
+  ]);
+  console.log(movieAll);
 }
 
-downloadAll()
+downloadAll();
 
 // Part C – Fastest with Promise.race
 
-
-async function downloadRase(){
-    const movieRace = await Promise.race([downloadMovie1(), downloadMovie2(), downloadMovie3()])
-    console.log(movieRace)
+async function downloadRase() {
+  const movieRace = await Promise.race([
+    downloadMovie1(),
+    downloadMovie2(),
+    downloadMovie3()
+  ]);
+  console.log(movieRace);
 }
 
-downloadRase()
+downloadRase();
